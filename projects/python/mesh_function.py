463f08d62d16f0065c2dc442c0083804fbe65e28
Iver Oknes <iver@oknes.no>
2023-09-01 13:36:44 +0200

    Add problem 1.1

--- projects/python/mesh_function.py
+++ projects/python/mesh_function.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 
-def mesh_function(f, t):
-    pass
+def mesh_function(f: callable, t: np.ndarray):
+    f_array = np.zeros_like(t, dtype="float64")
+    for i, val in enumerate(t):
+        f_array[i] = f(val)
+    return f_array
 
 def func(t):
-    pass
+    t = np.array(t, dtype="float64")
+    return np.piecewise(t, [(0 <= t) * (t <= 3), (3 < t) * (t <= 4)], [lambda x: np.exp(-x), lambda x: np.exp(-3 * x)])
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
