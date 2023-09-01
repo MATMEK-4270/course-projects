0aa1ff9a814d1ff773ec26ff5e2ca3547c252f2f
Iver Oknes <iver@oknes.no>
2023-09-01 13:52:05 +0200

    Add problem 1.2

--- projects/python/differentiate.py
+++ projects/python/differentiate.py
@@ -2,10 +2,25 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    d = np.zeros_like(u, dtype="float64")
+
+    d[0] = (u[1] - u[0]) / dt
+
+    for i in range(1, len(d) - 1):
+        d[i] = (u[i+1] - u[i-1]) / (2 * dt)
+
+    d[-1] = (u[-1] - u[-2]) / dt
+
+    return d
 
 def differentiate_vector(u, dt):
-    pass
+    d = np.zeros_like(u, dtype="float64")
+
+    d[0] = (u[1] - u[0]) / dt
+    d[1:-1] = (u[2:] - u[:-2]) / (2 * dt)
+    d[-1] = (u[-1] - u[-2]) / dt
+
+    return d
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
